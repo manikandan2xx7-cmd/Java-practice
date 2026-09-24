23d2e782ee1ec26da9d4345dff9b3b5104b1229b
Manikandan <manikandan2xx7@gmail.com>
2026-09-24 17:45:08 +0530

    update Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("enter first number : ");
+        int a=scanner.nextInt();
+        System.out.println("enter second number : ");
+        int b=scanner.nextInt();
+        System.out.println("ADDITION = "+(a+b));
+        System.out.println("SUBSTRACTION = "+(a-b));
+        System.out.println("MULTIPLICATION = "+(a*b));
+        System.out.println("DIVISION = "+(a/b));
+    }
+}
